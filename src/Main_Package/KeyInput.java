cf0aff9affc8639546ef4d8dff4a303000d0250c
dimamik <dymitrmikielewicz@gmail.com>
2020-07-24 12:12:22 +0200

    Fixed KeyInput

--- src/Main_Package/KeyInput.java
+++ src/Main_Package/KeyInput.java
@@ -2,13 +2,18 @@ package Main_Package;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
+/* 
+Fixed the switching bug by adding an array of booleans that catches the movements
+*/
 public class KeyInput extends KeyAdapter {
 
     private Handler handler;
-
+    private boolean[] keyDown = new boolean[4];
     public KeyInput(Handler handler) {
         this.handler = handler;
+        for (int i = 0; i < 4; i++) {
+            keyDown[i]=false;
+        }
     }
 
     public void keyPressed(KeyEvent e) {
@@ -22,15 +27,19 @@ public class KeyInput extends KeyAdapter {
 
                 if (key == KeyEvent.VK_W || key == 38) {
                     tmpObject.setVelY(-5);
+                    keyDown[0]=true;
                 }
                 if (key == KeyEvent.VK_S || key == 40) {
                     tmpObject.setVelY(5);
+                    keyDown[1]=true;
                 }
                 if (key == KeyEvent.VK_D || key == 39) {
                     tmpObject.setVelX(5);
+                    keyDown[2]=true;
                 }
                 if (key == KeyEvent.VK_A || key == 37) {
                     tmpObject.setVelX(-5);
+                    keyDown[3]=true;
                 }
 
             }
@@ -52,18 +61,24 @@ public class KeyInput extends KeyAdapter {
 
                 // key events to do while button is not re
                 if (key == KeyEvent.VK_W || key == 38) {
-                    tmpObject.setVelY(0);
+                    keyDown[0]=false;
                 }
                 if (key == KeyEvent.VK_S || key == 40) {
-                    tmpObject.setVelY(0);
+                    keyDown[1]=false;
+
                 }
                 if (key == KeyEvent.VK_D || key == 39) {
-                    tmpObject.setVelX(0);
+                    keyDown[2]=false;
+
                 }
                 if (key == KeyEvent.VK_A || key == 37) {
-                    tmpObject.setVelX(0);
-                }
+                    keyDown[3]=false;
 
+                }
+                //Vertical movement
+                if (!keyDown[0] && !keyDown[1]) tmpObject.setVelY(0);
+                //Horizontal movement
+                if (!keyDown[2] && !keyDown[3]) tmpObject.setVelX(0);
             }
         }
     }
